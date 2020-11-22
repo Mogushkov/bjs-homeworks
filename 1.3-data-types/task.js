09c73ad68ef0107da4b0b03d3be5cc21da7d8579
Gapur Mogushkov <gapur@mail.ru>
2020-11-22 22:30:10 +0300

    task 1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,20 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (isNaN(percent)) {
+        return "Параметр Процентная ставка содержит неправильное значение ${percent}";
+    }
+    else if (isNaN(contribution)) {
+        return "Параметр Первоначальный взнос содержит неправильное значение ${contribution}";
+    }
+    else if (isNaN(amount)) {
+        return "Параметр Сумма кредита содержит неправильное значение ${amount}";
+    }
+    let percentRate = (percent / 100) / 12;
+    let period = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth()); 
+    let currentPay = (amount - contribution) * (percentRate + percentRate / ((Math.pow((1 + percentRate), period)) - 1)); 
+    let totalAmount = amount + (currentPay * period - amount);
+    return Number(totalAmount.toFixed(2));  
 }
 
 function getGreeting(name) {
