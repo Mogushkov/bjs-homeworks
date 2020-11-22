b5b6f534b803b0afcc24577b7ed557a071a9947c
Gapur Mogushkov <gapur@mail.ru>
2020-11-22 19:41:57 +0300

    task 1

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,16 @@
+"use strict";
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let discriminant = Math.pow(b,2) - 4 * a * c;
+    let result = [];
+    
+    if (discriminant >= 0) {
+        result.push(( -b + Math.sqrt(discriminant) ) / ( 2 * a ));
+    }
+    if (discriminant > 0) {
+        result.push(( -b - Math.sqrt(discriminant) ) / ( 2 * a ));
+    }
+
+    return result;
 }
 
 function getAverageMark(marks){
