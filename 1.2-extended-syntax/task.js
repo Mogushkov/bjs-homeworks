f429826b249cb1de9c72cb0de80955fc3c2a0624
Gapur Mogushkov <gapur@mail.ru>
2020-11-26 21:52:53 +0300

    all

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -14,26 +14,32 @@ function getResult(a,b,c){
 }
 
 function getAverageMark(marks){
-    let result = [];
-    if (marks.length === 0) {
-        return 0;
-    }
-    else {
-        let result = marks.splice(0, 5);
-        let amount = (result.reduce((a, b) => (a + b)) / result.length);
-        return amount;
-    }
+    if (marks.length > 5) {
+        marks.splice(5);
+     
+
+     } if (marks.length == 0) {
+         return 0;
+     } 
+
+     let sumOfMarks = 0;
+      for (let i = 0; i < marks.length; i++) {
+             sumOfMarks += marks[i]; 
+         }
+         let averageMark = sumOfMarks / marks.length;
+
+         return averageMark;
 }
 
 function askDrink(name, dateOfBirthday){
-    let userName = name;
+    
     let userYear = dateOfBirthday.getFullYear();
     let currentDate = new Date().getFullYear();
     let userAge = currentDate - userYear;
     if (userAge > 18) {
-        return "Не желаете ли олд-фэшн, ${userName}?"
+        return `Не желаете ли олд-фэшн, ${name}?`
     }
     else {
-        return "Сожалею, ${userName}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!"
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`
     }
 }
